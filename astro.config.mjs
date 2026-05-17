33c95be5db3c4e3d5bd79da7863d644d40596d65
docomomo <133315191+docomola@users.noreply.github.com>
2026-05-17 16:07:39 +0800

    simplify: remove integrations for initial build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://docomola.github.io',
   base: '/myblog',
-  integrations: [mdx(), sitemap()],
 });
