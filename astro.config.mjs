b0ab579cddac18f144a19f3c740c4cbdc08af495
docomomo <133315191+docomola@users.noreply.github.com>
2026-05-17 15:21:14 +0800

    fix: update astro.config.mjs for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://docomola.github.io',
+	base: '/myblog',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
